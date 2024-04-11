94fe5f7ff2944b1fc0de7e4f43db5590f6659c73
SridharCheppala <sricheppala@gmail.com>
2024-04-10 23:11:48 -0500

    updated

--- controllers/electronic.js
+++ controllers/electronic.js
@@ -23,8 +23,16 @@ exports.electronic_view_all_Page = async function (req, res) {
     }
 };
 // for a specific electronic.
-exports.electronic_detail = function (req, res) {
-    res.send('NOT IMPLEMENTED: electronic detail: ' + req.params.id);
+// for a specific Costume.
+exports.electronic_detail = async function (req, res) {
+    console.log("detail" + req.params.id)
+    try {
+        result = await electronic.findById(req.params.id)
+        res.send(result)
+    } catch (error) {
+        res.status(500)
+        res.send(`{"error": document for id ${req.params.id} not found`);
+    }
 };
 // Handle electronic create on POST.
 exports.electronic_create_post = async function (req, res) {
@@ -52,6 +60,90 @@ exports.electronic_delete = function (req, res) {
     res.send('NOT IMPLEMENTED: electronic delete DELETE ' + req.params.id);
 };
 // Handle electronic update form on PUT.
-exports.electronic_update_put = function (req, res) {
-    res.send('NOT IMPLEMENTED: electronic update PUT' + req.params.id);
+// Handle Costume update form on PUT.
+exports.electronic_update_put = async function (req, res) {
+    console.log(`update on id ${req.params.id} with body
+${JSON.stringify(req.body)}`)
+    try {
+        let toUpdate = await electronic.findById(req.params.id)
+        // Do updates of properties
+        if (req.body.electronic_type)
+            toUpdate.electronic_type = req.body.electronic_type;
+        if (req.body.ratting) toUpdate.ratting = req.body.ratting;
+        if (req.body.cost) toUpdate.cost = req.body.cost;
+        let result = await toUpdate.save();
+        console.log("Sucess " + result)
+        res.send(result)
+    } catch (err) {
+        res.status(500)
+        res.send(`{"error": ${err}: Update for id ${req.params.id}
+failed`);
+    }
 };
+// Handle Costume delete on DELETE.
+exports.electronic_delete = async function(req, res) {
+console.log("delete " + req.params.id)
+try {
+result = await electronic.findByIdAndDelete( req.params.id)
+console.log("Removed " + result)
+res.send(result)
+} catch (err) {
+res.status(500)
+res.send(`{"error": Error deleting ${err}}`);
+}
+};
+// Handle a show one view with id specified by query
+exports.electronic_view_one_Page = async function(req, res) {
+    console.log("single view for id " + req.query.id)
+    try{
+    result = await electronic.findById( req.query.id)
+    res.render('electronicdetail',
+    { title: 'electronic Detail', toShow: result });
+    }
+    catch(err){
+    res.status(500)
+    res.send(`{'error': '${err}'}`);
+    }
+    };
+    // Handle building the view for creating a costume.
+// No body, no in path parameter, no query.
+// Does not need to be async
+exports.electronic_create_Page = function(req, res) {
+    console.log("create view")
+    try{
+    res.render('electroniccreate', { title: 'electronic Create'});
+    }
+    catch(err){
+    res.status(500)
+    res.send(`{'error': '${err}'}`);
+    }
+    };
+    // Handle building the view for updating a costume.
+// query provides the id
+exports.electronic_update_Page = async function(req, res) {
+    console.log("update view for item "+req.query.id)
+    try{
+    let result = await electronic.findById(req.query.id)
+    res.render('electronicupdate', { title: 'electronic Update', toShow: result });
+    }
+    catch(err){
+    res.status(500)
+    res.send(`{'error': '${err}'}`);
+    }
+    };
+    // Handle a delete one view with id from query
+exports.electronic_delete_Page = async function(req, res) {
+    console.log("Delete view for id " + req.query.id)
+    try{
+    result = await electronic.findById(req.query.id)
+    res.render('electronicdelete', { title: 'electronic Delete', toShow:
+    result });
+    }
+    catch(err){
+    res.status(500)
+    res.send(`{'error': '${err}'}`);
+    }
+    };
+    
+    
+    
--- routes/electronic.js
+++ routes/electronic.js
@@ -3,4 +3,9 @@ const electronic_controlers= require('../controllers/electronic');
 var router = express.Router();
 /* GET electronic */
 router.get('/', electronic_controlers.electronic_view_all_Page );
+/* GET detail costume page */
+router.get('/detail', electronic_controlers.electronic_view_one_Page);
+router.get('/create', electronic_controlers.electronic_create_Page);
+router.get('/update', electronic_controlers.electronic_update_Page);
+router.get('/delete', electronic_controlers.electronic_delete_Page);
 module.exports = router;
