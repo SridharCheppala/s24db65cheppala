5ff2f717f5e2f354422d537c351319391840beb1
SridharCheppala <sricheppala@gmail.com>
2024-04-16 21:05:49 -0500

    final

--- models/electronic.js
+++ models/electronic.js
@@ -1,8 +1,17 @@
 const mongoose = require("mongoose")
 const electronicSchema = mongoose.Schema({
 electronic_type: String,
-ratting: String,
-cost: Number
+ratting: {
+    type: String,
+    minlength: 1,
+    maxlength: 18,
+},
+cost:
+{
+    type: Number,
+    min : 1,
+    max :300,
+}
 })
 module.exports = mongoose.model("electronic",
 electronicSchema)
--- routes/electronic.js
+++ routes/electronic.js
@@ -1,4 +1,5 @@
 var express = require('express');
+var passport = require('passport');
 const electronic_controlers= require('../controllers/electronic');
 var router = express.Router();
 /* GET electronic */
@@ -6,6 +7,20 @@ router.get('/', electronic_controlers.electronic_view_all_Page );
 /* GET detail costume page */
 router.get('/detail', electronic_controlers.electronic_view_one_Page);
 router.get('/create', electronic_controlers.electronic_create_Page);
-router.get('/update', electronic_controlers.electronic_update_Page);
-router.get('/delete', electronic_controlers.electronic_delete_Page);
+//router.get('/update', electronic_controlers.electronic_update_Page);
+//router.get('/delete', electronic_controlers.electronic_delete_Page);
+// A little function to check if we have an authorized user and continue on or
+// redirect to login.
+const secured = (req, res, next) => {
+if (req.user){
+return next();
+}
+res.redirect("/login");
+}
+router.get('/update', secured,electronic_controlers.electronic_update_Page);
+router.get('/delete',secured, electronic_controlers.electronic_delete_Page);
+router.post('/login', passport.authenticate('local'), function(req, res) {
+    res.redirect('/');
+    });
+    
 module.exports = router;
